f0a29b895c98a85cf3e0fc840bd22dd0c3bdda87
moshe <mosheperl7375@gmail.com>
2022-11-21 17:51:21 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+document.getElementById('.bar') .onclick = function(){
+    <style>
+        .menu-one {
+            transform: rotate(45deg);
+        }
+        .menu-two {
+            transform: rotate(135deg);
+            position: absolute;
+            top: 0;
+        }
+    </style>
+}
