d79eba470ae4f3a022d4e24d4a816278e9adbfc3
moshe <mosheperl7375@gmail.com>
2023-01-07 21:50:53 -0600

    new commit

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 document.getElementById('body') .onscroll = () => {
-    console.log(scrollY)
     if(scrollY >= 675){
         document.querySelector('.sub-buffer') .style = 'width: 90%';
     } else {
         document.querySelector('.sub-buffer') .style = 'width: 0%'
+    }
+    if(scrollY >= 325){
+        document.querySelector('.about') .style = 'transform: scale(1.0);'
     }
 
     if(scrollY >= 1800){
@@ -11,4 +13,10 @@ document.getElementById('body') .onscroll = () => {
     } else {
         document.querySelector('.sub-buffer-two') .style = 'width: 0%'
     }
+}
+
+const icon = document.querySelector('.menu-icon');
+
+icon .onclick = () => {   
+    icon .classList.toggle('menu-icon-animate');
 }
